09bab113df8801c2a392114d881c05196d4d21be
Dylan Jay <software@pretaweb.com>
2010-12-13 17:31:12 +1100

    fix browser layer

--- pretaweb/pretamap/browser/interfaces.py
+++ pretaweb/pretamap/browser/interfaces.py
@@ -1,6 +1,7 @@
 from plone.theme.interfaces import IDefaultPloneLayer
+from zope.interface import Interface
 
 
-class IPretaMapLayer(IDefaultPloneLayer):
+class IPretaMapLayer(Interface):
     """Marker interface that defines a Zope 3 browser layer.
     """
